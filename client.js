9b6551911a76c446fcd7cedaf8d33822dc52c260
Ashutosh Verma <ashutosh@Ashutoshs-MacBook-Air.local>
2025-09-07 23:20:18 +0530

    docker commands

--- client.js
+++ client.js
@@ -4,3 +4,9 @@ exports.kafka = new Kafka({
   clientId: 'my-app',
   brokers: ['localhost:9092'], // connect to your local Kafka broker
 });
+
+// Start Kafka + Zookeeper
+// docker-compose up -d
+
+// Verify Kafka is running
+// docker ps
